ec1ff1b506e7d23af77fe369610e65da473f6d9a
Fireblade <72758695+Firebladedoge229@users.noreply.github.com>
2024-12-11 20:51:15 -0500

    Update FastFlagManager.cs

--- Bloxstrap/FastFlagManager.cs
+++ Bloxstrap/FastFlagManager.cs
@@ -90,9 +90,9 @@ namespace Bloxstrap
             { "New (2022)", "OldTexturePack" }
         };
 
-        public static IReadOnlyDictionary<string, string> TextureQuality => new Dictionary<string, string>
+        public static IReadOnlyDictionary<string, string?> TextureQuality => new Dictionary<string, string>
         {
-            { "Automatic", "None" },
+            { "Automatic", null },
             { "Level 0 (Lowest)", "0" },
             { "Level 1", "1" },
             { "Level 2", "2" },
